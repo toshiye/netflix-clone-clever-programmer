e919cad92f66146b5f91fc9a7772d3eeba01521c
toshiye <sasakikenji@gmail.com>
2021-03-02 11:37:25 -0300

    sasaki20210302

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "7064f6e781ef1b7eae727ae31ca805fb"
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-us`,
+    fecthActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fecthComdeyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fecthHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fecthRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fecthDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default requests
